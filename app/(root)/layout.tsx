596814fa169233d802ae10bc86deb634f244d375
PhDBui <congdoan6997@gmail.com>
2025-01-12 02:54:33 +0300

    feat: 🎸 add home layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,15 @@
+import Header from '@/components/Header';
 import { ReactNode } from 'react';
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
-  return <main className="root-container">{children}</main>;
+  return (
+    <main className="root-container">
+      <div className="mx-auto max-w-7xl">
+        <Header />
+        <div className="mt-20 pb-20">{children}</div>
+      </div>
+    </main>
+  );
 };
 
 export default RootLayout;
